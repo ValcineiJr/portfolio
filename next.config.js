f45eb0470ca958f81e727ac3b633d4e8fa14d680
Valcinei Jr <hakjog@gmail.com>
2022-05-24 23:46:27 -0300

    fix: tentando consertar o erro do cors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://valcineijr-portfolio.vercel.app/:path*',
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     domains: ['media.giphy.com', 'github.com', 'lh3.googleusercontent.com'],
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:3000/api/`,
+  baseURL: `${process.env.NEXT_PUBLIC_DB_HOST}api/`,
 });
 
 export default api;
